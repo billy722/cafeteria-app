289c168b6ed99238ff3e5d614b26888e166a9a8f
billy722 <bill_722@hotmail.com>
2025-05-07 22:10:17 -0400

    Prueba servicio render

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Hello World desde el Cliente Dev</h1>
+      <h1>Hello World desde el Cliente Main</h1>
     </div>
   );
 }
