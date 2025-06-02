1282c4e50ce0cc006d0e2c772018278241226851
billy722 <bill_722@hotmail.com>
2025-06-02 02:37:36 -0400

    Cambios en ventas

--- client2/src/pages/Ventas/Ventas.jsx
+++ client2/src/pages/Ventas/Ventas.jsx
@@ -1,8 +1,28 @@
+import { useState } from "react";
+import './Ventas.css';
 
 function Ventas(){
 
+    const [mesa, setMesa] = useState(0);
+
     return(
-        <h3>Pagina de ventas</h3>
+        <>
+            <h3>Seleccione Mesa</h3>
+
+            <ul>
+                <button onClick={() => setMesa(1)}>1</button>
+                <button onClick={() => setMesa(2)}>2</button>
+                <button onClick={() => setMesa(3)}>3</button>
+                <button onClick={() => setMesa(4)}>4</button>
+                <button onClick={() => setMesa(5)}>5</button>
+                <button onClick={() => setMesa(6)}>6</button>
+                <button onClick={() => setMesa(7)}>7</button>
+                <button onClick={() => setMesa(8)}>8</button>
+                <button onClick={() => setMesa(0)}>Mesón</button>
+            </ul>
+
+            <h3>{mesa === 0 ? "Mesón" : `Mesa ${mesa}`}</h3>
+        </>
     );
 }
 
